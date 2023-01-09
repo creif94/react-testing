1b37f183654cc532276f3a732729520b939e7e1f
Christopher <christopher94.reif@gmail.com>
2023-01-08 18:17:24 -0600

    Just finished section 1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App'; // import needed to display App
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />); // Render method creates a virtual dom,
+//   const linkElement = screen.getByText(/learn react/i); // Will find the element that is displayed,
+//   // (i)--> Case insensitive
+//   // /learn react/i regular expression that is case-insensitive
+//   // could use 'Learn React'
+//   expect(linkElement).toBeInTheDocument(); // assertion that causes test to succeed or fail -- Jest-Dom
+//   // .toBeInTheDocument() is a function that can be used to find the variable you defined earlier
+// });
+
+// Section 2: Simple App Tests
+test ('Correct initial color for Butotn', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  // Will test to seee if that the button will have the string Change to blue
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({background: 'red'})
+})
+test('button action turns  it blue when clicked', ()=>{
+  render(<App/>)
+  const colorButton = screen.getByRole('button', {name: 'Change to red'})
+
+})
+
